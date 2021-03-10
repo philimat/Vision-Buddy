3824f91e2df06a54c6878686bb7aa2d2c2fd2948
Matt Philippi <philippi.matthew@gmail.com>
2021-03-09 18:00:49 -0600

    Initial Commit

---
+++ Vision Buddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vision Buddy
+//
+//  Created by Matt Philippi on 3/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vision Buddy/Vision_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_BuddyApp.swift
+//  Vision Buddy
+//
+//  Created by Matt Philippi on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Vision_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
